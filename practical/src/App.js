0015086d2fd6c56f0132da27bcc02468e14577e2
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2024-11-27 19:42:01 +0530

    Create Login file

--- practical/src/App.js
+++ practical/src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import Login from './components/Login';
 import Dasboard from './components/Dasboard';
-import { BrowserRouter as Router, Routes, Route, Link,NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
+     
       </Routes>
     </Router>
   );
